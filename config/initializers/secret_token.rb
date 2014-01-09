bfabf17013d29880a92c42575b8c485822553949
Masataka <masataka2222222@gmail.com>
2014-01-09 22:24:19 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsWerckerHeroku::Application.config.secret_key_base = 'be47ffb6a543bbf14f907ab0ebe5c42662e106c249bf9f8c0ddbe5699d14086c8525679c1a25de2c25dd2bc3415dfc910db26ea1daad6d2e71227b24fb117e07'
